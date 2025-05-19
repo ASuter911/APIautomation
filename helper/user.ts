93709f54f38a8e0d387137a88b611c797e669ec2
Anna <anna-suter85@gmx.ch>
2025-05-19 21:31:14 +0200

    fix1 (#5)

--- helper/user.ts
+++ helper/user.ts
@@ -1,8 +1,7 @@
 import { faker } from "@faker-js/faker";
 import { User } from "./interface";
-import* as supertest from 'supertest'
-const request = supertest('http://localhost:8001/api/v1/')
-
+import * as supertest from "supertest";
+const request = supertest("http://localhost:8001/api/v1/");
 
 /*const userData:UserData = {
     "name": faker.person.fullName(),
@@ -10,83 +9,87 @@ const request = supertest('http://localhost:8001/api/v1/')
     "password": "test1234",
     "passwordConfirm": "test1234",
 };*/
-export function getUser(role: string):User {
-const randomUser = createRandomUser()
-const password = "test1234"
-return{
+export function getUser(role: string): User {
+  const randomUser = createRandomUser();
+  const password = "test1234";
+  return {
     name: randomUser.username,
     email: randomUser.email.toLowerCase(),
     password: password,
     passwordConfirm: password,
-    role:role
-}
+    role: role,
+  };
 }
 // Function to generate random user data
 export function createUser(): Partial<User> {
-    return {
-      name: faker.person.firstName(),
-      email: faker.internet.email(),
-      password: "Test@12345",
-      passwordConfirm: "Test@12345",
-    };
-  }
-export function createRandomUser(){
-        return {
-          userId: faker.string.uuid(),
-          username: faker.internet.username(), // before version 9.1.0, use userName()
-          email: faker.internet.email(),
-          avatar: faker.image.avatar(),
-          password: faker.internet.password(),
-          birthdate: faker.date.birthdate(),
-          registeredAt: faker.date.past(),
-        };
-      }
-      //signup user (promise with async/await)
-export function signUp(user:User):Promise<any> {
-    return new Promise((resolve, reject) => {
-        request.post('users/signup').send(user).end((err,res) => {
-            if (err)reject(err)
-            else resolve(res)
-        })
-    })
+  return {
+    name: faker.person.firstName(),
+    email: faker.internet.email(),
+    password: "Test@12345",
+    passwordConfirm: "Test@12345",
+  };
+}
+export function createRandomUser() {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(), // before version 9.1.0, use userName()
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+//signup user (promise with async/await)
+export function signUp(user: User): Promise<any> {
+  return new Promise((resolve, reject) => {
+    request
+      .post("users/signup")
+      .send(user)
+      .end((err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+  });
 }
 export function signUp2(user) {
-    return request.post('users/signup').send(user).expect(201)
+  return request.post("users/signup").send(user).expect(201);
 }
-export function login(user:User):Promise<any> {
-    return new Promise((resolve, reject) => {
-        request.post('users/login').send(user).end((err,res) => {
-            if (err)reject(err)
-            else resolve(res)
-        })
-    })
+export function login(user: User): Promise<any> {
+  return new Promise((resolve, reject) => {
+    request
+      .post("users/login")
+      .send(user)
+      .end((err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+  });
 }
 
-<<<<<<< HEAD
-export function login2(user){
-    return request.post('users/signup').send(user).expect(201)
-=======
-export function login2(user:User){
-    return request.post('users/login').send(user)
->>>>>>> main
-    }
+export function login2(user: User) {
+  return request.post("users/login").send(user);
+}
 
-    export function deleteFunction(cookie:string):Promise<any> {
-        return new Promise((resolve, reject) => {
-            request.delete('users/deleteMe').set('Cookie', cookie).end((err,res) => {
-                if (err)reject(err)
-                else resolve(res)
-            })
-        })
-    }
-    export function deleteFunction2(cookie:string) {
-        return request.delete('users/deleteMe').set('Cookie', cookie).expect(200)
-        }
+export function deleteFunction(cookie: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    request
+      .delete("users/deleteMe")
+      .set("Cookie", cookie)
+      .end((err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+  });
+}
+export function deleteFunction2(cookie: string) {
+  return request.delete("users/deleteMe").set("Cookie", cookie).expect(200);
+}
 
-    export function updateUser(cookie:string) {
-        return request.patch('users/updateMe').set('Cookie', cookie).send({name: "John Doe"}).expect(200)
-
-    }
-    
-
-   
+export function updateUser(cookie: string) {
+  return request
+    .patch("users/updateMe")
+    .set("Cookie", cookie)
+    .send({ name: "John Doe" })
+    .expect(200);
+}
--- src/test/Tour/create_tour_positive.spec.ts
+++ src/test/Tour/create_tour_positive.spec.ts
@@ -3,24 +3,20 @@ let cookie: string;
 /*import * as supertest from 'supertest'
 const request = supertest("http://localhost:8001/api/v1");*/
 
-describe('CREATE TOUR', () => {
-    it('create tour', async() => {
-        const userImport = getUser('admin');
-        console.log(userImport, 'userImport');
-        await signUp(userImport).then((res) => {
-            expect(res.statusCode).toBe(201);
-            expect(res.body.data.user.email).toEqual(userImport.email.toLowerCase());
-            cookie = res.headers['set-cookie'][0].split(';')[0];
-        });
-        await request 
-        .post('/tours')
-        .set('Cookie', cookie)
-        .send({
-<<<<<<< HEAD
-            name: "TourForn38",
-=======
-            name: "TourForn31",
->>>>>>> main
+describe("CREATE TOUR", () => {
+  it("create tour", async () => {
+    const userImport = getUser("admin");
+    console.log(userImport, "userImport");
+    await signUp(userImport).then((res) => {
+      expect(res.statusCode).toBe(201);
+      expect(res.body.data.user.email).toEqual(userImport.email.toLowerCase());
+      cookie = res.headers["set-cookie"][0].split(";")[0];
+    });
+    await request
+      .post("/tours")
+      .set("Cookie", cookie)
+      .send({
+        name: "Test tour345",
         duration: 10,
         description: "Could be",
         maxGroupSize: 10,
@@ -33,14 +29,14 @@ describe('CREATE TOUR', () => {
         guides: [],
         startDates: ["2024-04-04"],
         startLocation: {
-            type: "Point",
-            coordinates: [-74.005974, 40.712776], // [longitude, latitude]
+          type: "Point",
+          coordinates: [-74.005974, 40.712776], // [longitude, latitude]
         },
-}).then (tourRes => {
-console.log(tourRes.body, 'tourRes');
-expect(tourRes.statusCode).toBe(201);
-expect(tourRes.body.data.difficulty).toBe("easy");
-
-})
-})
-})
+      })
+      .then((tourRes) => {
+        console.log(tourRes.body, "tourRes");
+        expect(tourRes.statusCode).toBe(201);
+        expect(tourRes.body.data.difficulty).toBe("easy");
+      });
+  });
+});
